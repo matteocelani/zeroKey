78799f34c696900dc4003d0d9f5936bfc42934c7
Saul <andrea_salvemini@yahoo.it>
2024-10-04 22:38:49 +0200

    proj id

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,28 +3,14 @@ import React, { Fragment, useEffect } from 'react';
 import { useAccount } from 'wagmi';
 //Importing Components
 import Meta from '@/components/Meta';
-import ThemeSwitch from '@/components/ThemeSwitch';
 
 export default function Home() {
   const { address, isConnected } = useAccount();
 
-  useEffect(() => {
-    if (isConnected) {
-      console.log('Wallet address: ', address);
-    } else {
-      console.log('Not connected');
-    }
-  }, [address, isConnected]);
-
   return (
     <Fragment>
       <Meta />
-
-      <div className="w-full flex flex-col">
-        <div className="flex justify-center mt-8">
-          <ThemeSwitch />
-        </div>
-      </div>
+      <div className="w-full flex flex-col"></div>
     </Fragment>
   );
 }
