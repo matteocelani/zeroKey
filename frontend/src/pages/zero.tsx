e4fd6ec0321259654300843c0d63ad2b71560563
Matteo Celani <26189482+matteocelani@users.noreply.github.com>
2024-10-05 15:09:00 +0200

    Fix conditional components

--- frontend/src/pages/zero.tsx
+++ frontend/src/pages/zero.tsx
@@ -186,6 +186,7 @@ export default function Zero() {
         onPrevStep={handlePrevStep}
         onNextStep={handleNextStep}
         onFinish={handleGenerateProof}
+        isRecover
       />
     );
   };
